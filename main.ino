2e8e8e453336e157bd8e5b6bc9485945dbd4849c
Ruchitha Jujjuru <83824520+ruchitha1405@users.noreply.github.com>
2022-08-23 09:43:13 +0530

    added grid eye and esp32CAM code to project.ino

--- main.ino
+++ main.ino
@@ -4,11 +4,30 @@
 #include "HTTPClient.h"
 #include "time.h"
 #include <Melopero_AMG8833.h>
-
 Melopero_AMG8833 grid_sensor;
+#include "esp_camera.h"
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
 
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#include "camera_pins.h"
+#define camPin1 13
+#define camPin2 14
 #define pirPin 19
 #define motionLed 18
+# SCL_pin is GPIO22 in esp32
+# SDA_pin is GPIO21 in esp32
+
 bool isDetected;
 float t;
 String  motion;
@@ -17,6 +36,8 @@ String  motion;
 /*Put your SSID & Password*/
 const char *ssid = "redmi"; // Enter SSID here
 const char *pwd = "09262525";  //Enter Password here
+
+void startCameraServer();
 
 String cse_ip = "192.168.1.7"; // Do ifconfig to get your ip.
 String cse_port = "8080";
@@ -45,9 +66,13 @@ WebServer server(80);
 void send_event(const char *event);
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
   delay(100);
-  
+  pinMode(pirPin, INPUT);
+  pinMode(motionLed, OUTPUT);
  //********** GRID EYE CODE
   // initializing I2C to use default address AMG8833_I2C_ADDRESS_B and Wire (I2C-0):
   Wire.begin();
@@ -63,17 +88,70 @@ void setup()
   statusCode = grid_sensor.setFPSMode(FPS_MODE::FPS_10);
   Serial.println(grid_sensor.getErrorDescription(statusCode));
 //******** GRID EYE CODE
-
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  pinMode(pirPin, INPUT);
-  pinMode(motionLed, OUTPUT);
-  
   digitalWrite(motionLed, LOW);
+  //**********CAM code
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(camPin1, INPUT_PULLUP);
+  pinMode(camPin2, INPUT_PULLUP);
+#endif
+  // camera init
+  esp_err_t cam_err = esp_camera_init(&config);
+  if (cam_err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", cam_err);
+    return;
+  }
+  
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif 
+  //***********CAM code
   
   // Serial.println("--------------- Welcome -----------------");
- 
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
   //connect to your local wi-fi network
   WiFi.begin(ssid, pwd);
 
@@ -87,8 +165,12 @@ void setup()
   Serial.println("WiFi connected..!");
   Serial.print("Got IP: ");
   Serial.println(WiFi.localIP());
-
-  
+//*******CAM code
+  startCameraServer();
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+//*******CAM code  
   server.begin();
   Serial.println("HTTP server started");
 }
