20a7f6c6da9d089d546cc629f27e2a840cde46f0
Nandini MudhiReddy <82887305+nandinireddy3@users.noreply.github.com>
2022-09-02 11:58:12 +0530

    Create CameraWebServer.ino

---
+++ Project_esw/CameraWebServer.ino
@@ -0,0 +1,213 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+#include <esp32cam.h>
+#include "HTTPClient.h"
+#include "time.h"
+
+#include <Melopero_AMG8833.h>
+Melopero_AMG8833 grid_sensor;
+
+//*******CAM codef
+#include "esp_camera.h"
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#include "camera_pins.h"
+#define camPin1 13
+#define camPin2 14
+//*******CAM code
+
+#define pirPin 19 // pirOutput pin
+#define motionLed 18
+
+// SCL_pin is GPIO22 in esp32
+// SDA_pin is GPIO21 in esp32
+#define timeSeconds 1 // interval at which to blink led(seconds)
+
+
+int ledState = LOW; // ledState used to set the LED
+int pirState = LOW;
+// Timer: Auxiliar variables
+long now = millis();
+long lastTrigger = 0;
+boolean startTimer = false;
+int val = LOW;
+
+
+/*Put your SSID & Password*/
+const char *WIFI_SSID = "Galaxy M511CCF"; // Enter SSID here
+const char *WIFI_PASS = "ramreddy@3";  //Enter Password here
+
+WebServer server(80);
+//void startCameraServer();
+
+static auto loRes = esp32cam::Resolution::find(320, 240);
+static auto midRes = esp32cam::Resolution::find(350, 530);
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+ 
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+ 
+void handleJpgLo()
+{
+  if (!esp32cam::Camera.changeResolution(loRes)) {
+    Serial.println("SET-LO-RES FAIL");
+  }
+  serveJpg();
+}
+ 
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+ 
+void handleJpgMid()
+{
+  if (!esp32cam::Camera.changeResolution(midRes)) {
+    Serial.println("SET-MID-RES FAIL");
+  }
+  serveJpg();
+}
+
+void setup()
+{
+  
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  delay(100);
+  pinMode(pirPin, INPUT);
+  pinMode(motionLed, OUTPUT);
+  digitalWrite(motionLed, ledState);
+  
+ //********** GRID EYE CODE
+  // initializing I2C to use default address AMG8833_I2C_ADDRESS_B and Wire (I2C-0):
+  Wire.begin();
+  grid_sensor.initI2C();
+  // To use Wire1 (I2C-1):
+  // Wire1.begin();
+  // sensor.initI2C(AMG8833_I2C_ADDRESS_B, Wire1);
+  Serial.print("Resetting grid_sensor ... ");  
+  int statusCode = grid_sensor.resetFlagsAndSettings();
+  Serial.println(grid_sensor.getErrorDescription(statusCode));
+
+  Serial.print("Setting FPS ... ");
+  statusCode = grid_sensor.setFPSMode(FPS_MODE::FPS_10);
+  Serial.println(grid_sensor.getErrorDescription(statusCode));
+//******** GRID EYE CODE
+  
+  // Serial.println("--------------- Welcome -----------------");
+  Serial.println("Connecting to ");
+  Serial.println(WIFI_SSID);
+  Serial.println();
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+ 
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+//    Serial.println("WiFi Connected!!");
+  }
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cam-hi.jpg");
+  Serial.println("  /cam-mid.jpg");
+ 
+  server.on("/cam-lo.jpg", handleJpgLo);
+  server.on("/cam-hi.jpg", handleJpgHi);
+  server.on("/cam-mid.jpg", handleJpgMid);
+ 
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop()
+{
+   server.handleClient();
+  
+  
+  val = digitalRead(pirPin);  // read pir value
+  if (val == HIGH){
+    digitalWrite(motionLed, HIGH);
+    ledState = HIGH;
+    startTimer = true;
+    lastTrigger = millis();
+    if(pirState == LOW){
+      Serial.println("MOTION DETECTED!!!");
+      pirState = HIGH;
+    }
+  }
+  else{
+    pirState=LOW;
+    now = millis();
+    if(startTimer==true) {
+      if((now - lastTrigger) > (timeSeconds*1000)){
+        Serial.println("Motion stopped...");
+        digitalWrite(motionLed, LOW);
+        ledState = LOW;
+        startTimer = false;
+      }
+      else {
+        digitalWrite(motionLed, HIGH);
+        ledState = HIGH;
+      }
+    }
+  }
+// if duration after "motion stopped"  and next line is >=10 sec . then it is considered as motion stopped really, otherwise motion is not really stopped.
+  
+  
+  
+  //***************GRID EYE CODE
+  Serial.print("Updating grid_eye thermistor temperature ... ");
+  int statusCode = grid_sensor.updateThermistorTemperature();
+  Serial.println(grid_sensor.getErrorDescription(statusCode));
+
+  Serial.print("Updating pixel matrix ... ");
+  statusCode = grid_sensor.updatePixelMatrix();
+  Serial.println(grid_sensor.getErrorDescription(statusCode));
+
+  Serial.print("Thermistor temp: ");
+  Serial.print(grid_sensor.thermistorTemperature);
+  Serial.println("°C");
+
+  Serial.println("Temperature Matrix: ");
+  for (int x = 0; x < 8; x++){
+    for (int y = 0; y < 8; y++){
+      Serial.print(grid_sensor.pixelMatrix[y][x]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+
+  delay(1000);
+  //***************GRID EYE CODE
+}
+ 
