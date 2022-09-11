f825fab96e43c410067d31088a4560dee7fdee51
Nandini MudhiReddy <82887305+nandinireddy3@users.noreply.github.com>
2022-09-11 12:22:30 +0530

    added cam code

--- om2m_local.ino
+++ om2m_local.ino
@@ -5,6 +5,10 @@
 #include <WiFiClient.h>
 #include "HTTPClient.h"
 #include "time.h"
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "Base64.h"
 
 #include <Melopero_AMG8833.h>
 Melopero_AMG8833 grid_sensor;
@@ -65,12 +69,36 @@ const char *pwd = "12345678";  //Enter Password here
 //const char *ssid = "esw-m19@iiith"; // Enter SSID here
 //const char *pwd = "e5W-eMai@3!20hOct";  //Enter Password here
 
-WebServer server(80);
-//void startCameraServer();
+// https://script.google.com/macros/s/AKfycbxUyS-RZ-7dKZ9NswutjR8q9kdm1xOP9nMO6eGxNntD4jjzup1Z1rjpYMR0WNUPTrSi/exec
+String myScript = "/macros/s/AKfycbxUyS-RZ-7dKZ9NswutjR8q9kdm1xOP9nMO6eGxNntD4jjzup1Z1rjpYMR0WNUPTrSi/exec";    //Create your Google Apps Script and replace the "myScript" path.
+String myLineNotifyToken = "myToken=**********";    //Line Notify Token. You can set the value of xxxxxxxxxx empty if you don't want to send picture to Linenotify.
+String myFoldername = "&myFoldername=ESP32-CAM";
+String myFilename = "&myFilename=ESP32-CAM.jpg";
+String myImage = "&myFile=";
 
-//static auto loRes = esp32cam::Resolution::find(320, 240);
-//static auto midRes = esp32cam::Resolution::find(350, 530);
-//static auto hiRes = esp32cam::Resolution::find(800, 600);
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+WebServer server(80);
 //************** OM2M *****************
 #define CSE_IP     "192.168.36.221"// "127.0.0.1"//"esw-onem2m.iiit.ac.in"//"192.168.171.221"//////192.168.36.221 //replace with system-ip  // //esw-onem2m.iiit.ac.in
 #define CSE_PORT   5089// 443 //5089 //443
@@ -82,50 +110,6 @@ WebServer server(80);
 
 
 //************** OM2M *****************
-
-//void serveJpg()
-//{
-//  auto frame = esp32cam::capture();
-//  if (frame == nullptr) {
-//    Serial.println("CAPTURE FAIL");
-//    server.send(503, "", "");
-//    return;
-//  }
-//  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-//                static_cast<int>(frame->size()));
-// 
-//  server.setContentLength(frame->size());
-//  server.send(200, "image/jpeg");
-//  WiFiClient client = server.client();
-//  frame->writeTo(client);
-//}
- 
-//void handleJpgLo()
-//{
-//  if (!esp32cam::Camera.changeResolution(loRes)) {
-//    Serial.println("SET-LO-RES FAIL");
-//  }
-//  serveJpg();
-//}
-// 
-//void handleJpgHi()
-//{
-//  if (!esp32cam::Camera.changeResolution(hiRes)) {
-//    Serial.println("SET-HI-RES FAIL");
-//  }
-//  serveJpg();
-//}
-// 
-//void handleJpgMid()
-//{
-//  if (!esp32cam::Camera.changeResolution(midRes)) {
-//    Serial.println("SET-MID-RES FAIL");
-//  }
-//  serveJpg();
-//  // capturing
-//}
-
-
 
 void setup()
 {
@@ -154,21 +138,11 @@ void setup()
   Serial.println(grid_sensor.getErrorDescription(statusCode));
 //******** GRID EYE CODE
   
-  // Serial.println("--------------- Welcome -----------------");
+  Serial.println("--------------- Welcome -----------------");
   Serial.println("Connecting to ");
   Serial.println(ssid);
   Serial.println();
- // {
-//    using namespace esp32cam;
-//    Config cfg;
-//    cfg.setPins(pins::AiThinker);
-//    cfg.setResolution(hiRes);
-//    cfg.setBufferCount(2);
-//    cfg.setJpeg(80);
-// 
-//    bool ok = Camera.begin(cfg);
-//    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
-//  }
+
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pwd);
@@ -178,14 +152,51 @@ void setup()
   }
   Serial.print("http://");
   Serial.println(WiFi.localIP());
-  Serial.println("  /cam-lo.jpg");
-  Serial.println("  /cam-hi.jpg");
-  Serial.println("  /cam-mid.jpg");
- 
-//  server.on("/cam-lo.jpg", handleJpgLo);
-//  server.on("/cam-hi.jpg", handleJpgHi);
-//  server.on("/cam-mid.jpg", handleJpgMid);
- 
+  
+camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    //ESP.restart();
+  }
+  else {
+  //drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_VGA);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  }
   server.begin();
   Serial.println("HTTP server started");
   ThingSpeak.begin(client); 
@@ -275,8 +286,6 @@ void om2m_createCI(int & pirState, String & row01,String & row23,String & row45,
 
 
 void loop(){
-
-
    server.handleClient();
   
   val = digitalRead(pirPin);  // read pir value
@@ -372,10 +381,123 @@ void loop(){
        Serial.println("Problem updating channel. HTTP error code " + String(x));
      }
      //********** Thingspeak **********
+     SendCapturedImage();
      count = 0;
  }
    delay(1000);
    count++;
    //delay(30000);
+}
+
+String SendCapturedImage() {
+  const char* myDomain = "script.google.com";
+  String getAll="", getBody = "";
+  
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+    return "Camera capture failed";
+  }  
+  
+  Serial.println("Connect to " + String(myDomain));
+  WiFiClientSecure client_tcp;
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+  
+  if (client_tcp.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+    
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    String imageFile = "data:image/jpeg;base64,";
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+    String Data = myLineNotifyToken+myFoldername+myFilename+myImage;
+    
+    client_tcp.println("POST " + myScript + " HTTP/1.1");
+    client_tcp.println("Host: " + String(myDomain));
+    client_tcp.println("Content-Length: " + String(Data.length()+imageFile.length()));
+    client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+    client_tcp.println("Connection: keep-alive");
+    client_tcp.println();
+    
+    client_tcp.print(Data);
+    int Index;
+    for (Index = 0; Index < imageFile.length(); Index = Index+1000) {
+      client_tcp.print(imageFile.substring(Index, Index+1000));
+    }
+    esp_camera_fb_return(fb);
+    
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTime = millis();
+    boolean state = false;
+    
+    while ((startTime + waitTime) > millis())
+    {
+      Serial.print(".");
+      delay(100);      
+      while (client_tcp.available()) 
+      {
+          char c = client_tcp.read();
+          if (state==true) getBody += String(c);        
+          if (c == '\n') 
+          {
+            if (getAll.length()==0) state=true; 
+            getAll = "";
+          } 
+          else if (c != '\r')
+            getAll += String(c);
+          startTime = millis();
+       }
+       if (getBody.length()>0) break;
+    }
+    client_tcp.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connected to " + String(myDomain) + " failed.";
+    Serial.println("Connected to " + String(myDomain) + " failed.");
+  }
+  
+  return getBody;
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
 }
  
